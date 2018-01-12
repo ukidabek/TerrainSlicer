e4f25053021d46db9f0de733bef1b59798c00447
ukidabek <ukidabek@gmail.com>
2018-01-12 21:22:32 +0100

    - Terrain slicer update.

---
+++ Assets/Plugins/TerrainSlicer/Editor/TerrainSlicer.cs
@@ -0,0 +1,192 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System.Collections;
+using System.Collections.Generic;
+
+public class TerrainSlicer : EditorWindow
+{
+    private int _fromGUIHorizontalSlices = 2;
+    private int _fromGUIVerticalSlices = 2;
+
+    private int _horizontalSlices = 2;
+    private int _verticalSlices = 2;
+
+    private int horizontalSize = 0;
+    private int verticallSize = 0;
+
+    private int _heightMapPreviewSize = 513;
+
+    private Terrain _terrain = null;
+    private TerrainData _terrainData
+    {
+        get
+        {
+            if (_terrain == null)
+                return null;
+
+            return _terrain.terrainData;
+        }
+    }
+
+    private Texture2D _heightMap = null;
+    private Texture2D[,] _slicedHeightMap = null;
+
+    [MenuItem("Window/TerrainSlicer")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        TerrainSlicer window = (TerrainSlicer)GetWindow(typeof(TerrainSlicer));
+        window.Show();
+    }
+
+    private void OnSelectionChange()
+    {
+        if (Selection.activeObject == null)
+        {
+            _terrain = null;
+            return;
+        }
+
+        _terrain = (Selection.activeObject as GameObject).GetComponent<Terrain>();
+        GenerateHeightmapTexture();
+        Repaint();
+    }
+
+    private void GenerateHeightmapTexture()
+    {
+        int heightmapWidth = _terrainData.heightmapWidth;
+        int heightmapHeight = _terrainData.heightmapHeight;
+
+        int _height = ((_terrainData.heightmapWidth - 1) / _horizontalSlices) + 1;
+
+        float[,] hights = _terrainData.GetHeights(0, 0, heightmapWidth, heightmapHeight);
+        _heightMap = new Texture2D(heightmapWidth, heightmapHeight);
+
+        for (int x = 0; x < heightmapWidth; x++)
+        {
+            for (int y = 0; y < heightmapHeight; y++)
+            {
+                float height = hights[x, y];
+                Color color = new Color(height, height, height);
+                _heightMap.SetPixel(x, y, color);
+            }
+        }
+
+        _heightMap.Apply();
+    }
+
+    public void Slice()
+    {
+        _horizontalSlices = _fromGUIHorizontalSlices;
+        _verticalSlices = _fromGUIHorizontalSlices;
+
+        _slicedHeightMap = new Texture2D[_horizontalSlices, _verticalSlices];
+
+        int heightmapWidth = _terrainData.heightmapWidth;
+        int heightmapHeight = _terrainData.heightmapHeight;
+
+        horizontalSize = heightmapWidth / _horizontalSlices;
+        verticallSize = heightmapHeight / _verticalSlices;
+
+
+        for (int i = 0; i < _horizontalSlices; i++)
+        {
+            for (int j = 0; j < _verticalSlices; j++)
+            {
+                _slicedHeightMap[i,j] = new Texture2D(horizontalSize, verticallSize);
+            }
+        }
+
+        int sliceHorizontal = 0;
+        int sliceVertical = 0;
+
+        for (int i = _horizontalSlices - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < _verticalSlices; j++)
+            {
+                Color[] color = _heightMap.GetPixels(sliceHorizontal, sliceVertical, horizontalSize, verticallSize);
+                _slicedHeightMap[i, j].SetPixels(0, 0, horizontalSize, verticallSize, color);
+
+                sliceHorizontal += horizontalSize;
+            }
+            sliceVertical += verticallSize;
+            sliceHorizontal = 0;
+        }
+
+        for (int i = 0; i < _horizontalSlices; i++)
+        {
+            for (int j = 0; j < _verticalSlices; j++)
+            {
+                _slicedHeightMap[i, j].Apply();
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        if(_terrain == null)
+        {
+            EditorGUILayout.LabelField("Select terrain!");
+            return;
+        }
+
+        EditorGUILayout.BeginHorizontal();
+        {
+            EditorGUILayout.BeginVertical();
+            {
+                EditorGUILayout.FloatField("Width", _terrainData.size.x);
+                EditorGUILayout.FloatField("Length", _terrainData.size.z);
+                EditorGUILayout.FloatField("Height", _terrainData.size.y);
+
+                _fromGUIHorizontalSlices = EditorGUILayout.IntField("Horizontal Slices", _fromGUIHorizontalSlices);
+                _fromGUIVerticalSlices = EditorGUILayout.IntField("Vertical Slices", _fromGUIVerticalSlices);
+                if(GUILayout.Button("Slice"))
+                {
+                    Slice();
+                }
+
+            }
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.BeginVertical();
+            {
+                GUILayout.Label(_heightMap, GUILayout.Width(_heightMapPreviewSize));
+            }
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.BeginVertical();
+            {
+                if(_slicedHeightMap != null)
+                {
+                    GUIStyle style = new GUIStyle();
+                    style.padding = new RectOffset(0, 0, 0, 0);
+                    style.margin = new RectOffset(0, 0, 0, 0);
+                    style.fixedWidth = _heightMapPreviewSize / _horizontalSlices;
+                    style.fixedHeight = _heightMapPreviewSize / _verticalSlices;
+
+                    EditorGUILayout.BeginVertical();
+                    {
+                        for (int i = 0; i < _horizontalSlices; i++)
+                        {
+                            EditorGUILayout.BeginHorizontal();
+                            {
+                                for (int j = 0; j < _verticalSlices; j++)
+                                {
+                                    if(GUILayout.Button(_slicedHeightMap[i, j], style))
+                                    {
+                                        Debug.Log(i + " " + j);
+                                    }
+                                }
+                            }
+                            EditorGUILayout.EndHorizontal();
+                        }
+                    }
+                    EditorGUILayout.EndVertical();
+                }
+            }
+            EditorGUILayout.EndVertical();
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+}
---
+++ Assets/Plugins/TerrainSlicer/TerainDataLog.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerainDataLog : MonoBehaviour
+{
+    [SerializeField]
+    Terrain _terrain = null;
+
+    [SerializeField]
+    Texture2D[] _map = null;
+
+    [SerializeField]
+    Texture2D _hightMap = null;
+
+    [SerializeField]
+    Texture2D[] _newHigntMaps = null;
+
+    [SerializeField]
+    float[,] _hights = null;
+
+    private void Awake()
+    {
+        TerrainData data = _terrain.terrainData;
+        _map = data.alphamapTextures;
+
+        int xx = (((int)data.heightmapWidth - 1)/2)+1;
+        int yy = (((int)data.heightmapHeight - 1)/2)+1;
+
+        _newHigntMaps = new Texture2D[4];
+
+        for (int i = 0; i < 4; i++)
+        {
+            _newHigntMaps[i] = new Texture2D(xx, yy);
+        }
+
+        _hightMap = new Texture2D((int)data.heightmapWidth, (int)data.heightmapHeight);
+        _hights = data.GetHeights(0,0, (int)data.heightmapWidth, (int)data.heightmapHeight);
+        for (int x = 0; x < data.heightmapWidth; x++)
+        {
+            for (int y = 0; y < data.heightmapHeight; y++)
+            {
+                float height = _hights[x, y];
+                Color color = new Color(height, height, height);
+                _hightMap.SetPixel(x, y, color);
+            }
+        }
+        _hightMap.Apply();
+
+        int xxx = 0;
+        int yyy = 0;
+        for (int x = 0; x < (int)data.heightmapWidth; x++)
+        {
+            float u = (float)x / (int)data.heightmapWidth;
+            for (int y = 0; y < (int)data.heightmapHeight; y++)
+            {
+                float v = (float)y / (int)data.heightmapHeight;
+
+                Color color = _hightMap.GetPixelBilinear(u, v);
+                if(x <= xx && y <= xx)
+                {
+                    _newHigntMaps[0].SetPixel(x, y, color);
+                }
+            }
+        }
+        _newHigntMaps[0].Apply();
+
+
+        Debug.Log(data.size.ToString());
+        Debug.Log(_hights[90,24].ToString());
+
+        //int x = 1;
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    x *= 2;
+        //    Debug.Log(x);
+        //}
+    }
+}
